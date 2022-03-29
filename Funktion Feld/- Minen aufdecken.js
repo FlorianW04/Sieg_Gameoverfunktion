262d53471a8fd801f2b3dd542f0c6c39223878d9
wickerfl <florian10wickert@gmail.com>
2022-03-29 13:56:16 +0200

    vermint, minen aufdecken

---
+++ Funktion Feld/- Minen aufdecken.js
@@ -0,0 +1,31 @@
+function mineAufdecken()
+{
+
+
+
+
+}
+
+function feldAufdecken()
+{
+
+
+
+}
+var Click = function (feldAufdecken)
+{
+    if(!Zustand = 1)
+
+
+
+
+        zelleGeöffnet = true;
+    if (Nachbarzellenzähler > 0 )
+
+
+
+
+
+
+
+}
---
+++ vermint.js
@@ -0,0 +1,13 @@
+var istVermint = function (1,2,3)
+{
+    var Zelle = 1[2 + "" + 3];
+    var vermint = 0;
+    if (typeof zelle !== 'undefined')
+
+    {
+        mined = Zelle.vermint ? 1 : 0;
+    }
+        return vermint;
+
+
+}
